5334b8b8714af66eba4b7e2df3fdd1eb36328462
Leonardo <lupadinho867@gmail.com>
2023-07-22 11:49:13 -0300

    criando o hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src("./src/styles/*.scss")
@@ -7,7 +8,15 @@ function styles() {
         .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function imgs() {
+    return gulp.src("./src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/img"));
+}
+
+
+exports.default = gulp.parallel(styles, imgs);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+
 }
